d986819a2e8b193b9908d73cf9dcd369ba22068b
Steven Sanborn <stevensanborn@gmail.com>
2024-09-13 18:14:39 -1000

    fix hydration error
    Signed-off-by: Steven Sanborn <stevensanborn@gmail.com>

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -1,22 +1,33 @@
 import { FC, ReactNode } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { ConnectionProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import * as web3 from '@solana/web3.js'
 import * as walletAdapterWallets from '@solana/wallet-adapter-wallets';
+import dynamic from 'next/dynamic';
 require('@solana/wallet-adapter-react-ui/styles.css');
+
+
+//prevent hydration error
+const WalletProviderDynamic = dynamic(
+    async () => (await import('@solana/wallet-adapter-react')).WalletProvider,
+    { ssr: false }
+);
+
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const endpoint = web3.clusterApiUrl('devnet')
 	const wallets = [new walletAdapterWallets.PhantomWalletAdapter()]
 
 	return (
+		
 		<ConnectionProvider endpoint={endpoint}>
-	    <WalletProvider wallets={wallets}>
-	      <WalletModalProvider>
-	        { children }
-        </WalletModalProvider>
-      </WalletProvider>
+	    <WalletProviderDynamic wallets={wallets}>
+	       <WalletModalProvider> 
+	        <>{ children  }</>
+         </WalletModalProvider>
+    </WalletProviderDynamic> 
     </ConnectionProvider>
+	
 	)
 }
 
